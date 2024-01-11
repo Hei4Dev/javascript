02b6493421a3442f58ce92f564144ea92c64806d
hei <heitorcassiano10@gmail.com>
2024-01-11 13:05:40 -0300

    Criei o exercício 015

---
+++ ex015/script.js
@@ -0,0 +1,47 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/bebe-h.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-h.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/adulto-h.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso-h.jpg')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'imagens/bebe-m.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'imagens/jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'imagens/adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'imagens/idoso-m.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
